622ca596b6af08fe7faa1e898155570e951df594
Akhil V <sonofhotmale@gmail.com>
2019-08-13 13:06:09 -0400

    made minor edits to utils.py to make it more pythonic

--- misc/utils.py
+++ misc/utils.py
@@ -43,7 +43,8 @@ def centroid(*args):
     microphone_array = np.zeros((len(args), len(args[1])))
 
     # Converts microphone locations into an array
-    for i in range(len(args)):
+    # for i in range(len(args)):
+    for i, item in enumerate(args):
         microphone_array[i, :] = np.array(args[i])
 
     # Finds the centroid
@@ -64,14 +65,14 @@ def create_microphone_locations_array():
     return microphone_locations
 
 
-def split_and_conquer(radius, centroid, arr, num_sources):
+def split_and_conquer(radius, the_centroid, arr, num_sources):
     """ Helper function used when looking for multiple sources. Breaks up the cartesian array into smaller bits
         in order to help multiply the radius and recenter.
 
         Keyword arguments:
 
             radius -- set of points we are using for our radius
-            centroid -- the specific centroid associated with each coordinate
+            the_centroid -- the specific centroid associated with each coordinate
             arr -- the cartesian array before the multiplication of the radius
             num_sources -- number of sources we are looking for
         """
@@ -84,16 +85,16 @@ def split_and_conquer(radius, centroid, arr, num_sources):
 
         # Multiply each respective part by the radius and recenter it with the centroid
         for i in range(num_sources):
-            array_split[i] = radius * array_split[i] + np.array (centroid)[np.newaxis, :]
+            array_split[i] = radius * array_split[i] + np.array(the_centroid)[np.newaxis, :]
 
     return np.vstack((array_split))
 
 
-def use_kd_tree(S1_bool, centerlist, total_array):
+def use_kd_tree(s1_bool, centerlist, total_array):
     """ Optional: Use the KD Tree Structure to find S1 and S2 sources.
 
         Keyword:
-            S1_bool -- boolean to determine whether to track for S1 or S2 sound source
+            s1_bool -- boolean to determine whether to track for S1 or S2 sound source
             centerlist -- center of the room
             total_array -- the array of the possible sources.
 
@@ -105,7 +106,7 @@ def use_kd_tree(S1_bool, centerlist, total_array):
     # Put the whole list into a tree data data structure
     tree = spatial.KDTree(total_array)
 
-    if S1_bool:
+    if s1_bool:
         # Find the points closest to where S1 sound is
         s1_indices = tree.query_ball_point([-0.0639405, -0.01994509, -0.02030148], 2.5e-2)
         potential_sources = np.array([total_array[s1_indices][j] for j in range(len(s1_indices))])
@@ -119,16 +120,15 @@ def use_kd_tree(S1_bool, centerlist, total_array):
         # return the potential S1 source and the S1 source
         return source, s_source
 
-    else:
-        # Find the point closest to where S2 sound is
-        s2_indices = tree.query_ball_point([-0.09080822, -0.03022343, 0.02206185], 2.5e-2)
-        potential_sources = np.array([total_array[s2_indices][j] for j in range(len(s2_indices))])
+    # Find the point closest to where S2 sound is
+    s2_indices = tree.query_ball_point([-0.09080822, -0.03022343, 0.02206185], 2.5e-2)
+    potential_sources = np.array([total_array[s2_indices][j] for j in range(len(s2_indices))])
 
-        # Reconvert all the potential source points
-        source = np.add(centerlist, potential_sources)  # Width, Depth, Length
+    # Reconvert all the potential source points
+    source = np.add(centerlist, potential_sources)  # Width, Depth, Length
 
-        # Recenter the S2 Source
-        s_source = np.add(centerlist, np.array([-0.09080822, -0.03022343, 0.02206185]))
+    # Recenter the S2 Source
+    s_source = np.add(centerlist, np.array([-0.09080822, -0.03022343, 0.02206185]))
 
-        # return the potential S2 source and the S2 source
-        return source, s_source
+    # return the potential S2 source and the S2 source
+    return source, s_source
